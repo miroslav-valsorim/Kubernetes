dc4099280cef54a4615d9b9e8ae5c63c4c30d13b
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-09-23 20:57:51 +0300

    advanced concepts ex2

--- 06-Advanced-Concepts/ex2/commands.sh
+++ 06-Advanced-Concepts/ex2/commands.sh
@@ -33,3 +33,35 @@ kubectl delete -f part2/1-auto-scale.yaml
 minikube addons disable metrics-server
 
 kubectl delete -f metrics-server.yaml
+
+kubectl get nodes --show-labels
+
+kubectl describe node k8s1
+
+kubectl describe node | grep Taints
+
+kubectl taint nodes k8s1 node-role.kubernetes.io/master:NoSchedule-
+
+kubectl get pods -n kube-system -o wide
+
+kubectl describe pod etcd-k8s1 -n kube-system
+
+kubectl describe pod coredns-<identifier> -n kube-system
+
+kubectl taint node k8s2 demo-taint=nomorework:NoSchedule
+
+kubectl describe node | grep Taints
+
+kubectl apply -f part2/2-schedule.yaml
+
+kubectl get pods -o wide
+
+kubectl delete -f part2/2-schedule.yaml
+
+kubectl apply -f part2/2-schedule-toleration.yaml
+
+kubectl get pods -o wide
+
+kubectl taint node k8s2 demo-taint-
+
+kubectl delete -f part2/2-schedule-toleration.yaml
