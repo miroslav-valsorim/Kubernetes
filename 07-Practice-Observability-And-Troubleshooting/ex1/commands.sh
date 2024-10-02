030b108b1ad38ba76dba49227e05c8b75e3dae29
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-10-02 22:21:51 +0300

    practice obs and troubleshooting ex1

--- 07-Practice-Observability-And-Troubleshooting/ex1/commands.sh
+++ 07-Practice-Observability-And-Troubleshooting/ex1/commands.sh
@@ -18,3 +18,53 @@ kubectl describe pod liveness-http
 kubectl get pod liveness-http
 
 kubectl delete -f part1/2-liveness-http.yaml
+
+# Readiness Probes
+
+kubectl apply -f part1/3-readiness-cmd.yaml
+
+kubectl describe pod readiness-cmd
+
+kubectl describe svc readiness-cmd
+
+kubectl describe svc readiness-cmd
+
+watch -n 2 kubectl get pods,svc -o wide
+
+kubectl delete -f part1/3-readiness-cmd.yaml
+
+# HTTP Probe
+
+kubectl apply -f part1/4-readiness-http.yaml
+
+kubectl describe pod readiness-http
+
+kubectl describe svc readiness-http
+
+kubectl describe svc readiness-http
+
+watch -n 2 kubectl get pods,svc -o wide
+
+kubectl delete -f part1/4-readiness-http.yaml
+
+# Startup Probes
+
+kubectl apply -f part1/5-startup-liveness-same.yaml
+
+kubectl logs startup-same
+
+kubectl describe pod startup-same
+
+kubectl delete -f part1/5-startup-liveness-same.yaml
+
+# Startup and Liveness Mixed Type
+
+kubectl apply -f part1/6-startup-liveness-mixed.yaml
+
+kubectl describe pod startup-mixed
+
+kubectl describe svc startup-mixed
+
+watch -n 2 kubectl get pods,svc -o wide
+
+kubectl delete -f part1/6-startup-liveness-mixed.yaml
