0cd2e1572ebc65d6ff1b90570d04229daa21c2b7
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-10-04 21:37:30 +0300

    practice obs and troubleshooting ex2

--- 07-Practice-Observability-And-Troubleshooting/ex2/commands.sh
+++ 07-Practice-Observability-And-Troubleshooting/ex2/commands.sh
@@ -2,3 +2,25 @@
 
 # Samples are either inspired and/or taken directly from here: 
 # https://kubernetes.io/docs/tasks/debug-application-cluster/debug-application-introspection/
+
+sudo mkdir /var/lib/k8s-audit
+
+sudo cp part2/1-audit-simple.yaml /var/lib/k8s-audit/
+
+sudo mkdir /var/log/k8s-audit
+
+sudo vi /etc/kubernetes/manifests/kube-apiserver.yaml
+
+kubectl get pods -n kube-system -w
+
+kubectl run logtest1 --image=alpine -- sleep 1d
+
+kubectl get pods
+
+cat /var/log/k8s-audit/audit.log | grep logtest1
+
+cat /var/log/k8s-audit/audit.log | grep logtest1 | jq
+
+cat /var/log/k8s-audit/audit.log | grep logtest1 | jq -s length
+
+kubectl delete pod logtest1
