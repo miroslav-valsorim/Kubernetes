d48a52bd79a42b9fa7ee2a899cd58d665bef4eea
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-10-05 22:59:52 +0300

    practice obs and troubleshooting ex2

--- 07-Practice-Observability-And-Troubleshooting/ex2/commands.sh
+++ 07-Practice-Observability-And-Troubleshooting/ex2/commands.sh
@@ -24,3 +24,22 @@ cat /var/log/k8s-audit/audit.log | grep logtest1 | jq
 cat /var/log/k8s-audit/audit.log | grep logtest1 | jq -s length
 
 kubectl delete pod logtest1
+
+sudo cp part2/2-audit.yaml /var/lib/k8s-audit/
+
+sudo vi /etc/kubernetes/manifests/kube-apiserver.yaml
+
+kubectl get pods -n kube-system -w
+
+kubectl run logtest2 --image=alpine -- sleep 1d
+
+kubectl get pods
+
+cat /var/log/k8s-audit/audit.log | grep logtest2 | jq
+
+cat /var/log/k8s-audit/audit.log | grep logtest2 | jq -s length
+
+kubectl delete pod logtest2
+
+# logging
+# https://kubernetes.io/docs/concepts/cluster-administration/logging/
